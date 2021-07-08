3a533fde6cbec7e7693ecef7d776a6a6fc70e462
Brian Ojoh Mile <brianmile10@yahoo.com>
2021-07-08 17:28:59 -0400

    init

---
+++ apiweatherapp/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className='flex flex-wrap bg-red-900 h-screen w-screen justify-center text-white'>
+      <div>vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv</div>
+    </div>
+  );
+}
+
+export default App;
